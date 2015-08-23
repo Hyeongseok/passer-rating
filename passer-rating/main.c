404c2f7ea35ae20ae523fd7913b296a4705d95da
Hyeongseok Kang <romanze76@hotmail.com>
2015-08-24 00:21:58 +0900

    Initial Commit

---
+++ passer-rating/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  passer-rating
+//
+//  Created by Hyeongseok Kang on 8/24/15.
+//  Copyright (c) 2015 Hyeongseok Kang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
